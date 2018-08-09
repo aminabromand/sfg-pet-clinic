30c768dba574653662161fdc53ffc5a621c980e5
amin <amin.r.abromand@gmail.com>
2018-08-09 16:50:14 +0200

    created bootstrap for data. Closes #14.

--- pet-clinic-data/src/main/java/abromand/train/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/abromand/train/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,8 +1,9 @@
 package abromand.train.sfgpetclinic.services.map;
 
 import abromand.train.sfgpetclinic.model.Vet;
+import abromand.train.sfgpetclinic.services.VetService;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Vet save(Vet vet) {
         return super.save( vet.getId(), vet );
