0f475ed3db2f3dece821945e88c3a9d52699a481
amin <amin.r.abromand@gmail.com>
2018-08-15 10:38:21 +0200

    Added specialities to vets on startup. cloases #34

--- pet-clinic-data/src/main/java/abromand/train/sfgpetclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/abromand/train/sfgpetclinic/services/map/VetMapService.java
@@ -1,9 +1,35 @@
 package abromand.train.sfgpetclinic.services.map;
 
+import abromand.train.sfgpetclinic.model.Speciality;
 import abromand.train.sfgpetclinic.model.Vet;
+import abromand.train.sfgpetclinic.services.SpecialityService;
 import abromand.train.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
+
+    private SpecialityService specialityService;
+
+    public VetMapService(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        Vet savedVet = null;
+        if(object != null) {
+            if (object.getSpecialities() != null) {
+                object.getSpecialities().forEach(speciality -> {
+                    if(speciality.getId() == null) {
+                        Speciality savedSpeciality = specialityService.save(speciality);
+                        speciality.setId(savedSpeciality.getId());
+                    }
+                });
+            }
+            return super.save(object);
+        } else {
+            return null;
+        }
+    }
 }
