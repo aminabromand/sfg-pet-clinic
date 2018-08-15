2c938c9fadb1fab33dfc5f4457361647afb8f860
amin <amin.r.abromand@gmail.com>
2018-08-15 10:07:32 +0200

    added Pets and Contact info to owners on startup. Closes #33

--- pet-clinic-data/src/main/java/abromand/train/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/abromand/train/sfgpetclinic/services/map/OwnerMapService.java
@@ -1,11 +1,48 @@
 package abromand.train.sfgpetclinic.services.map;
 
 import abromand.train.sfgpetclinic.model.Owner;
+import abromand.train.sfgpetclinic.model.Pet;
 import abromand.train.sfgpetclinic.services.OwnerService;
+import abromand.train.sfgpetclinic.services.PetService;
+import abromand.train.sfgpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerMapService(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        Owner savedOwner = null;
+        if(object != null) {
+            if (object.getPets() != null) {
+                object.getPets().forEach(pet -> {
+                    if (pet.getPetType() != null) {
+                        if(pet.getPetType().getId() == null) {
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    } else {
+                        throw new RuntimeException("Pet Type is required");
+                    }
+
+                    if(pet.getId() == null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(object);
+        } else {
+            return null;
+        }
+    }
 
     @Override
     public Owner findByLastName(String lastName) {
