468db6b5e85a5b48c9fbdc4f9a789fcdf1698066
Amin Abromand <amin.abromand@metaproc.com>
2018-08-24 14:51:54 +0200

    Added Visit Entity, closes #38

--- pet-clinic-data/src/main/java/abromand/train/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/abromand/train/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package abromand.train.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -20,6 +22,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "brith_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -52,4 +57,12 @@ public class Pet extends BaseEntity {
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
     }
+
+	public Set<Visit> getVisits(){
+		return visits;
+	}
+
+	public void setVisits( Set<Visit> visits ){
+		this.visits = visits;
+	}
 }
