e538467cc47de62a4799d5dad95c1ffd13150277
Amin Abromand <amin.abromand@metaproc.com>
2018-08-24 15:31:48 +0200

    created sping data jpa service for owners. closes #40

---
+++ pet-clinic-data/src/main/java/abromand/train/sfgpetclinic/services/springdatajpa/AbstractSDJpaService.java
@@ -0,0 +1,36 @@
+package abromand.train.sfgpetclinic.services.springdatajpa;
+
+import abromand.train.sfgpetclinic.model.BaseEntity;
+import abromand.train.sfgpetclinic.model.Owner;
+import abromand.train.sfgpetclinic.services.CrudService;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+abstract class AbstractSDJpaService<T extends BaseEntity, ID extends Long> implements CrudService<T, ID> {
+
+	protected CrudRepository<T, ID> crudRepository;
+
+	public Set<T> findAll(){
+		Set<T> found = new HashSet<>(  );
+		crudRepository.findAll().forEach( found::add );
+		return found;
+	}
+
+	public T findById( ID aLong ){
+		return crudRepository.findById( aLong ).orElse( null );
+	}
+
+	public T save( T object ){
+		return crudRepository.save( object );
+	}
+
+	public void delete( T object ){
+		crudRepository.delete( object );
+	}
+
+	public void deleteById( ID aLong ){
+		crudRepository.deleteById( aLong );
+	}
+}
